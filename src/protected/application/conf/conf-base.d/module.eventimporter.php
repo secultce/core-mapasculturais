da5e58c6320710202f974ccf3b95b1aae4ce1ea7
Daniel De Araujo Dantas <daniel.dantas@secult.local>
2023-01-17 08:57:16 -0300

    func-importador-de-eventos

--- src/protected/application/conf/conf-base.d/module.eventimporter.php
+++ src/protected/application/conf/conf-base.d/module.eventimporter.php
@@ -2,5 +2,13 @@
 
 return [
     'module.EventImporter' => [
+        'enable'=> function()
+        {
+            $app = MapasCulturais\App::i();
+            $allow = [101124,34866,26250,109563,109559,36280];
+            if(is_array($app->user->profile->id, $allow)){
+                return true;
+            }
+        }
     ]
 ];
